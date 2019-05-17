57a0660b7ec5f1202950a9b80d04bfbe58ea1391
scofeld <40675819+scofeld@users.noreply.github.com>
2019-05-18 02:24:02 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+let data = [
+	{
+		img:'img/first.png',
+		alt: 'img/1.png'
+	},
+	{
+		img:'img/nine.png',
+		alt: 'img/2.png'
+	},
+	{
+		img:'img/six.png',
+		alt: 'img/3.png'
+	},
+	{
+		img:'img/ten.png',
+		alt: 'img/41.png'
+	},
+	{
+		img:'img/eleven.png',
+		alt: 'img/1_2.png'
+	},
+	{
+		img:'img/twelve.png',
+		alt: 'img/2_2.png'
+	},
+	{
+		img:'img/thirteen.png',
+		alt: 'img/3_2.png'
+	},
+	{
+		img:'img/seven.png',
+		alt: 'img/4_2.png'
+	}
+]
+
